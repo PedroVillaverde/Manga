aa1566a545399536370920093d72ddf1f0032054
Administrador <Administrador@2DAM01>
2023-12-12 21:29:08 +0100

    arreglo nombre

--- src/readMangax/Micontrolador.java
+++ src/readMangax/Micontrolador.java
@@ -9,8 +9,7 @@ public class Micontrolador {
 	        this.vista = vista;
 
 	
-	        vista.gettxtNombre().setText("ReadMangax");
-	        vista.gettxtNombre().setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14));
+	      
     }
 }
 
